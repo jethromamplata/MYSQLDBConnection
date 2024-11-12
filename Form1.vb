12254d859065d73a6aed9df1d34f9a194fe7210b
CSAR1018026 <CSAR1018026@PL23>
2024-11-12 12:14:02 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Private Sub btnTest(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=test"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING      CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+End Class
